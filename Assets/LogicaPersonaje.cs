136324f0008c1f7224926058d7b450d465e064d0
Rtorrerso <79935998+Rtorrerso@users.noreply.github.com>
2021-05-04 19:59:02 -0500

    Primera version de importacion de personaje

---
+++ Assets/LogicaPersonaje.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje : MonoBehaviour
+{
+    public float velocidad = 5.0f;
+    public float rotacion = 200.0f;
+
+    public Animator anim;
+
+    public float x,y ;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0,x*Time.deltaTime*rotacion,0);
+        transform.Translate(0,0,y*Time.deltaTime*velocidad);
+
+        anim.SetFloat("velx",x);
+        anim.SetFloat("vely",y);
+    }
+}
